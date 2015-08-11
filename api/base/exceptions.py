cf837cb8fa0f2451a339ddb6d06e27c0ae03323c
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-08-11 13:36:25 -0400

    Add custom exception handler

---
+++ api/base/exceptions.py
@@ -0,0 +1,13 @@
+
+def jsonapi_exception_handler(exc, context):
+    """
+    Custom exception handler that returns errors object as an array with a 'detail' member
+    """
+    from rest_framework.views import exception_handler
+    response = exception_handler(exc, context)
+
+    # Return 401 instead of 403 during unauthorized requests without having user log in with Basic Auth
+    if response is not None and response.data['errors'][0]['detail'] == "Authentication credentials were not provided.":
+        response.status_code = 401
+
+    return response
