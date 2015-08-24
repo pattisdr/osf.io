ee736e0bf4d8fae340d52758210a819297220805
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-08-24 11:16:36 -0400

    Add space

--- api/base/language.py
+++ api/base/language.py
@@ -1,5 +1,6 @@
-from django.utils.translation import ugettext_lazy as _
 
+from django.utils.translation import ugettext_lazy as _
+ 
 REGISTER_WARNING = _('You are about to register {} '
                     'and everything that is inside it. Registration creates a permanent, '
                     'time-stamped, uneditable version of the project. If you would prefer to '
@@ -10,4 +11,5 @@ REGISTER_WARNING = _('You are about to register {} '
                     'There will no longer be the option of creating a permanently private '
                     'registration. If you register before June 8, 2015 and leave your '
                     'registration private, then the registration can remain private. After June 8, 2015, '
-                    'if you ever make it public, you will not be able to return it to private.  Please post to new URL to continue.')
+                    'if you ever make it public, you will not be able to return it to private.'
+                     'Please post to new URL to continue.')
