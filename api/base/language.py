86b1a3ebb8a4704f2a425c872c010684f106a5c9
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-08-26 10:49:21 -0400

    Modify REGISTER_WARNING and add EMBARGO_INFORMATION

--- api/base/language.py
+++ api/base/language.py
@@ -1,15 +1,19 @@
 
 from django.utils.translation import ugettext_lazy as _
 
-REGISTER_WARNING = _('You are about to register {} '
-                    'and everything that is inside it. Registration creates a permanent, '
-                    'time-stamped, uneditable version of the project. If you would prefer to '
-                    'register a particular component, please navigate to that component and then '
-                    'initiate registration. '
-                    'Important Note: Effective June 8, 2015, registrations '
-                    'will be made public immediately or can be embargoed for up to one year. '
-                    'There will no longer be the option of creating a permanently private '
-                    'registration. If you register before June 8, 2015 and leave your '
-                    'registration private, then the registration can remain private. After June 8, 2015, '
-                    'if you ever make it public, you will not be able to return it to private.'
-                     'Please post to new URL to continue.')
+
+REGISTER_WARNING = _('Registration cannot be undone, and the archived content and files cannot '
+                     'be deleted after registration. Please be sure the project is complete and '
+                     'comprehensive for what you wish to register. Post to new URL to continue.')
+
+EMBARGO_INFORMATION = _('You can choose whether to make your registration public immediately or embargo '
+                        'it for up to four years. At the end of the embargo period the registration is '
+                        'automatically made public. After becoming public, the only way to remove a '
+                        'registration is to retract it. Retractions show only the registration title, '
+                        'contributors, and description to indicate that a registration was made and '
+                        'later retracted.  If you choose to embargo your registration, a notification'
+                        ' will be sent to all other project contributors. Other administrators will have'
+                        ' 48 hours to approve or cancel creating the registration. If any other '
+                        'administrator rejects the registration, it will be canceled. If all other '
+                        'administrators approve or do nothing, the registration will be confirmed and '
+                        'enter its embargo period.')
