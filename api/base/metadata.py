f28faca9bce8e2f516b252f3cdc5ff4e66c81450
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-08-27 09:58:46 -0400

    Use OSF_META_SCHEMAS instead of pulling from MetaSchema to populate schemas in OPTIONS request

--- api/base/metadata.py
+++ api/base/metadata.py
@@ -1,6 +1,6 @@
 from rest_framework.metadata import SimpleMetadata
 
-from website.project.model import MetaSchema
+from website.project.metadata.schemas import OSF_META_SCHEMAS
 
 class SchemaMetadata(SimpleMetadata):
     """
@@ -8,8 +8,8 @@ class SchemaMetadata(SimpleMetadata):
     """
     def determine_metadata(self, request, view):
         metadata = super(SchemaMetadata, self).determine_metadata(request, view)
-        schema_choices = {schema.name: schema.schema for schema in MetaSchema.find()}
-
+        schema_choices = {schema['name'] + ' v' + str(schema['version']): schema for schema in OSF_META_SCHEMAS}
         metadata['registration_schema_choices'] = schema_choices
+
 
         return metadata
