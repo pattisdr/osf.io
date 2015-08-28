2b7e7757ce4f4cf3596b6d8299b866be70f7a5a3
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-08-28 09:30:36 -0400

    Document method override, expand docstring.

--- api/base/metadata.py
+++ api/base/metadata.py
@@ -2,10 +2,13 @@ from rest_framework.metadata import SimpleMetadata
 
 from website.project.metadata.schemas import OSF_META_SCHEMAS
 
+
 class SchemaMetadata(SimpleMetadata):
     """
-    Adds registration_schema_choices to OPTIONS
+    Adds registration_schema_choices to OPTIONS response.
     """
+
+    # Overrides SimpleMetadata
     def determine_metadata(self, request, view):
         metadata = super(SchemaMetadata, self).determine_metadata(request, view)
         schema_choices = {schema['name'] + ' v' + str(schema['version']): schema for schema in OSF_META_SCHEMAS}
