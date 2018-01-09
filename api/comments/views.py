0dc01b0abd40f3d46a763c831a7f90d8ef003f42
Dawn Pattison <pattison.dawn.r@gmail.com>
2018-01-09 11:08:09 -0600

    Now that comment target is WikiPage no need to separately consider BaseFileNode and WikiPage.

--- api/comments/views.py
+++ api/comments/views.py
@@ -21,7 +21,7 @@ from framework.auth.core import Auth
 from framework.auth.oauth_scopes import CoreScopes
 from framework.exceptions import PermissionsError
 from osf.models import AbstractNode, Comment, BaseFileNode
-from addons.wiki.models import WikiVersion
+from addons.wiki.models import WikiPage
 
 
 class CommentMixin(object):
@@ -167,11 +167,9 @@ class CommentDetail(JSONAPIBaseView, generics.RetrieveUpdateDestroyAPIView, Comm
 
         if isinstance(comment.target.referent, AbstractNode):
             comment_node = comment.target.referent
-        elif isinstance(comment.target.referent, BaseFileNode):
+        elif isinstance(comment.target.referent, (WikiPage,
+                                                 BaseFileNode)):
             comment_node = comment.target.referent.node
-        elif isinstance(comment.target.referent, WikiVersion):
-            comment_node = comment.target.referent.wiki_page.node
-
         if comment_node and comment_node.is_registration:
             self.serializer_class = RegistrationCommentDetailSerializer
 
